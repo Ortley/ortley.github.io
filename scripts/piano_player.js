502d8e5bd72b1a7ef3e40eb8e555acd62d95af0a
Ortley <etarlinguk@gmail.com>
2024-05-25 08:03:33 +0100

    init

---
+++ scripts/piano_player.js
@@ -0,0 +1,69 @@
+let isMouseDown = false;
+
+class Key {
+    static baseWidth = 10;
+    static spacing = 1;
+    static colorActive = "#ffffff";
+    static colorInActive = "#999999";
+
+    constructor (context, keyNote) {
+        this.context = context;
+
+        this.keyNote = keyNote;
+
+        this.isPlaying = false;
+    }
+
+    draw() {
+        this.isPlaying = Math.random() > 0.5;
+        this.context.fillStyle = this.isPlaying ? Key.colorInActive : Key.colorActive;
+        this.context.fillRect(this.keyNote * (Key.baseWidth + Key.spacing), 0, Key.baseWidth, 50)
+    };
+}
+
+class Piano {
+    static minNote = 0;
+    static maxNote = 100;
+    constructor (canvasId) {
+        this.canvas = document.getElementById(canvasId);
+        this.context = this.canvas.getContext("2d");
+        this.canvas.addEventListener("mousedown", this.mouseDown);
+        this.canvas.addEventListener("mouseup", this.mouseUp);
+
+        this.keyObjects = [];
+
+        this.createKeys();
+
+        window.requestAnimationFrame(() => this.loop());
+    }
+
+    createKeys() {
+        for(let note = Piano.minNote; note < (Piano.maxNote - Piano.minNote); note++) {
+            this.keyObjects.push(new Key(this.context, note));
+        }
+    }
+
+    mouseDown() {
+        isMouseDown = true;
+    }
+
+    mouseUp() {
+        isMouseDown = false;
+    }
+
+    loop() {
+        console.log("ok")
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for(let i = 0; 0 < this.keyObjects.length; i++) {
+            this.keyObjects[i].draw();
+        }
+        setTimeout( () => {
+            window.requestAnimationFrame(() => this.loop());
+        }, 100)
+    }
+}
+
+
+window.onload = function() {
+    let piano = new Piano("pianoCanvas");
+}
