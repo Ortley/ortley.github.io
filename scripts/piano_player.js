1136f1f0caea007a9225752124ca859ca102437d
Ortley <etarlinguk@gmail.com>
2024-05-25 08:05:17 +0100

    modified:   scripts/piano_player.js

--- scripts/piano_player.js
+++ scripts/piano_player.js
@@ -8,37 +8,35 @@ class Key {
 
     constructor (context, keyNote) {
         this.context = context;
-
         this.keyNote = keyNote;
-
         this.isPlaying = false;
     }
 
     draw() {
         this.isPlaying = Math.random() > 0.5;
         this.context.fillStyle = this.isPlaying ? Key.colorInActive : Key.colorActive;
-        this.context.fillRect(this.keyNote * (Key.baseWidth + Key.spacing), 0, Key.baseWidth, 50)
-    };
+        this.context.fillRect(this.keyNote * (Key.baseWidth + Key.spacing), 0, Key.baseWidth, 50);
+    }
 }
 
 class Piano {
     static minNote = 0;
     static maxNote = 100;
+
     constructor (canvasId) {
         this.canvas = document.getElementById(canvasId);
         this.context = this.canvas.getContext("2d");
-        this.canvas.addEventListener("mousedown", this.mouseDown);
-        this.canvas.addEventListener("mouseup", this.mouseUp);
+        this.canvas.addEventListener("mousedown", this.mouseDown.bind(this));
+        this.canvas.addEventListener("mouseup", this.mouseUp.bind(this));
 
         this.keyObjects = [];
-
         this.createKeys();
 
         window.requestAnimationFrame(() => this.loop());
     }
 
     createKeys() {
-        for(let note = Piano.minNote; note < (Piano.maxNote - Piano.minNote); note++) {
+        for(let note = Piano.minNote; note < Piano.maxNote; note++) {
             this.keyObjects.push(new Key(this.context, note));
         }
     }
@@ -52,17 +50,15 @@ class Piano {
     }
 
     loop() {
-        console.log("ok")
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for(let i = 0; 0 < this.keyObjects.length; i++) {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); // Clear the canvas
+        for(let i = 0; i < this.keyObjects.length; i++) {
             this.keyObjects[i].draw();
         }
-        setTimeout( () => {
+        setTimeout(() => {
             window.requestAnimationFrame(() => this.loop());
-        }, 100)
+        }, 100);
     }
 }
-
 
 window.onload = function() {
     let piano = new Piano("pianoCanvas");
