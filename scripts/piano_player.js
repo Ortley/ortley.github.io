5c45e7a66550d33ff5b63881b612613df8cefab7
Ortley <etarlinguk@gmail.com>
2024-05-26 17:20:38 +0100

    The visuals are there but I can't seem to get it to add the hover class

--- scripts/piano_player.js
+++ scripts/piano_player.js
@@ -1,13 +1,28 @@
-let toneKey = '<div class="tone-key"></div>';
+const toneKeyHTML = '<div class="tone-key"></div>';
+const semitoneKeyHTML = '<div class="semitone-key"></div>';
+const placeholderSemitoneKeyHTML = '<div class="semitone-key placeholder-key"></div>';
 
 window.addEventListener("DOMContentLoaded", function () {
-	let keyHtml = "<p>Hello World!</p>";
 	let container = document.getElementById("pianoContainer");
 	let toneContainer = document.getElementById("pianoTone");
 	let semitoneContainer = document.getElementById("pianoSemitone");
 
-	toneContainer.innerHTML += toneKey;
-	semitoneContainer.innerHTML = +toneKey;
+	for(n=0; n < 12; n++) {
+		toneContainer.innerHTML += toneKeyHTML;
+		let addedElement = toneContainer.lastElementChild;
 
-	console.log(container.childNodes);
+		addedElement.addEventListener("mouseover", function() {
+			console.log("ok");
+			document.getElementById("pianoTone").innerHTML = "";
+		})
+		if ((n % 7) == 2 || (n % 7) == 6){
+			semitoneContainer.innerHTML += placeholderSemitoneKeyHTML;
+		}
+		else {
+			semitoneContainer.innerHTML += semitoneKeyHTML;
+		}
+	}
+
+	toneContainer.lastElementChild.classList.add("hover");
+	toneContainer.lastElementChild.addEventListener("mouseover", function() {console.log("ok")});
 });
