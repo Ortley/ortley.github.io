675dbc103a2fd1f4787a852a8eed88210c1910c6
Ortley <etarlinguk@gmail.com>
2024-05-29 14:25:59 +0100

    I have no idea why the tones arent being detected :(

--- scripts/piano_player.js
+++ scripts/piano_player.js
@@ -2,28 +2,55 @@ const toneKeyHTML = '<div class="tone-key"></div>';
 const semitoneKeyHTML = '<div class="semitone-key"></div>';
 const placeholderSemitoneKeyHTML = '<div class="semitone-key placeholder-key"></div>';
 
+document.addEventListener("mouseover", (event) => {
+	console.log(event.target);
+})
+
 window.addEventListener("DOMContentLoaded", (event) => {
 	let container = document.getElementById("pianoContainer");
 	let toneContainer = document.getElementById("pianoTone");
 	let semitoneContainer = document.getElementById("pianoSemitone");
-	console.log("loaded")
 
-	for(n=0; n < 12; n++) {
+	for (n = 0; n < 12; n++) {
 		toneContainer.innerHTML += toneKeyHTML;
-		let addedElement = toneContainer.lastElementChild;
 
-		addedElement.addEventListener(
-			"mousemove",
-			(event) => {
-				console.log(event);
-			},
-			false
-		);
 		if ((n % 7) == 2 || (n % 7) == 6){
 			semitoneContainer.innerHTML += placeholderSemitoneKeyHTML;
 		}
 		else {
 			semitoneContainer.innerHTML += semitoneKeyHTML;
-		}
-	}
+		};
+	};
+
+	toneContainer.querySelectorAll(".tone-key").forEach((element) => {
+		element.addEventListener("mouseover", (event) => {
+			element.classList.add("hover");
+		});
+		element.addEventListener("mousedown", (event) => {
+			element.classList.add("active");
+		})
+		element.addEventListener("mouseup", (event) => {
+			element.classList.remove("active");
+		})
+		element.addEventListener("mouseout", (event) => {
+			element.classList.remove("hover");
+			element.classList.remove("active");
+		});
+	});
+
+	semitoneContainer.querySelectorAll(".semitone-key:not(.placeholder-key)").forEach(element => {
+		element.addEventListener("mouseover", (event) => {
+			element.classList.add("hover");
+		});
+		element.addEventListener("mousedown", (event) => {
+			element.classList.add("active");
+		})
+		element.addEventListener("mouseup", (event) => {
+			element.classList.remove("active");
+		})
+		element.addEventListener("mouseout", (event) => {
+			element.classList.remove("hover");
+			element.classList.remove("active");
+		});
+	});
 });
