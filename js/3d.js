d4ce3238bca3edb461487b4a6a2b7651a85b214e
Ortley <etarlinguk@gmail.com>
2024-08-11 17:19:25 +0100

    just realised that its wrong, redoing it all

--- js/3d.js
+++ js/3d.js
@@ -4,17 +4,28 @@ var ambientLight = {
     b: 90 / 256,
 };
 var lightColour = {
-    r: 255 / 256,
-    g: 255 / 256,
-    b: 255 / 256,
+    r: 256 / 256,
+    g: 256 / 256,
+    b: 256 / 256,
 };
 var albedo = {
-    r: 0 / 256,
-    g: 0 / 256,
-    b: 255 / 256,
+    r: 256 / 256,
+    g: 256 / 256,
+    b: 256 / 256,
 };
 
+var lightPosition = {
+    x: 0,
+    y: 0,
+    z: 200,
+}
+
+
 document.addEventListener("mousemove", e => {
+    updateAll(e);
+});
+
+function updateAll(e) {
     let element = document.getElementById("tri-content");
     let originRect = element.getBoundingClientRect();
     let origin = {
@@ -51,13 +62,13 @@ document.addEventListener("mousemove", e => {
     
     element.style.transform = `rotateY(${angle.y + Math.PI + Math.PI/2}rad)`;
     element.style.transform += `rotateX(${angle.x}rad)`;
-
+    element = document.getElementById("tri-content");
     let lightVector = {
-        x: 0 - origin.x,
-        y: 0 - origin.y,
-        z: 200 - origin.z,
+        x: lightPosition.x - origin.x,
+        y: lightPosition.y - origin.y,
+        z: lightPosition.z - origin.z,
     };
-    
+
     distance = Math.sqrt(lightVector.x * lightVector.x + lightVector.y * lightVector.y + lightVector.z * lightVector.z);
 
     lightVector = {
@@ -74,7 +85,9 @@ document.addEventListener("mousemove", e => {
         b: Math.min(256, Math.max(0, albedo.b * ambientLight.b)) + Math.min(256, Math.max(0, albedo.b * dotProduct * lightColour.b)),
     };
 
+    console.log(dotProduct);
+    console.log(finalColour.b);
+    
     
     element.style.backgroundColor = `color(srgb ${finalColour.r} ${finalColour.g} ${finalColour.b})`;
-
-});
+}
