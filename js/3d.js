bfaf2b98ad12222d469f1fa28a4989a843983d1c
Ortley <etarlinguk@gmail.com>
2024-08-26 15:32:11 +0100

    created some classes for 3d and moved the javascript into functions

--- js/3d.js
+++ js/3d.js
@@ -1,26 +1,75 @@
-var ambientLight = {
-    r: 90 / 256,
-    g: 90 / 256,
-    b: 90 / 256,
-};
-var lightColour = {
-    r: 256 / 256,
-    g: 256 / 256,
-    b: 256 / 256,
-};
-var albedo = {
-    r: 256 / 256,
-    g: 256 / 256,
-    b: 256 / 256,
+
+class vector3 {
+    static zero = new vector3(0, 0, 0);
+    static axisX = new vector3(1, 0, 0);
+    static axisY = new vector3(0, 1, 0);
+    static axisZ = new vector3(0, 0, 1);
+
+    constructor(x = 0, y = 0, z = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.length = 0;
+    };
 };
 
-var lightPosition = {
-    x: 0,
-    y: 0,
-    z: 200,
-}
+class basis3D {
+    constructor(x = new vector3.axisX, y = new vector3.axisY, z = new vector3.axisZ) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    };
+};
+
+class transform3D {
+    constructor(basis = new basis3D(), position = new vector3()) {
+        this.basis = basis;
+        this.position = position;
+
+        this.x = this.basis.x;
+        this.y = this.basis.y;
+        this.z = this.basis.z;
+        this.w = this.position;
+    };
+};
+
+start3D();
+
+function start3D() {
+
+    const ambientLight = {
+        r: 90 / 256,
+        g: 90 / 256,
+        b: 90 / 256,
+    };
+    const lightColour = {
+        r: 256 / 256,
+        g: 256 / 256,
+        b: 256 / 256,
+    };
+    const albedo = {
+        r: 256 / 256,
+        g: 256 / 256,
+        b: 256 / 256,
+    };
+
+    const lightPosition = {
+        x: 0,
+        y: 0,
+        z: 200,
+    };
+
+    var targetPosition = {
+        x: 0,
+        y: 0,
+        z: 200,
+    };
 
 
-document.addEventListener("mousemove", e => {
-    updateAll(e);
-});
+    var canvas = document.getElementById("3d-content");
+
+    document.addEventListener("mousemove", (e) => {
+        targetPosition.x = e.pageX;
+        targetPosition.y = e.pageY;
+    });
+};
--- js/piano_player.js
+++ js/piano_player.js
@@ -1,107 +1,111 @@
-const toneKeyHTML = '<div class="tone-key"></div>';
-const semitoneKeyHTML = '<div class="semitone-key"></div>';
-const placeholderSemitoneKeyHTML = '<div class="semitone-key placeholder-key"></div>';
+piano();
 
-var mouseClick = false;
-var activeKey = -1;
-var previousKey = -1;
-var containerScroll = -1;
-
-window.onload = function() {
-	MIDI.loadPlugin({
-		soundfontUrl: "./soundfont/",
-		instrument: "acoustic_grand_piano",
-		onprogress: function(state, progress) {
-			console.log(state, progress);
-		},
+function piano() {
+	const toneKeyHTML = '<div class="tone-key"></div>';
+	const semitoneKeyHTML = '<div class="semitone-key"></div>';
+	const placeholderSemitoneKeyHTML = '<div class="semitone-key placeholder-key"></div>';
+	
+	var mouseClick = false;
+	var activeKey = -1;
+	var previousKey = -1;
+	var containerScroll = -1;
+	
+	window.onload = function() {
+		MIDI.loadPlugin({
+			soundfontUrl: "./soundfont/",
+			instrument: "acoustic_grand_piano",
+			onprogress: function(state, progress) {
+				console.log(state, progress);
+			},
+		});
+	};
+	
+	document.onmousedown = function() {
+		mouseClick = true;
+	};
+	document.onmouseup = function() {
+		mouseClick = false;
+	};
+	
+	window.addEventListener("DOMContentLoaded", () => {
+		generatePiano(21, 108);
 	});
-};
-
-document.onmousedown = function() {
-	mouseClick = true;
-};
-document.onmouseup = function() {
-	mouseClick = false;
-};
-
-window.addEventListener("DOMContentLoaded", () => {
-	generatePiano(21, 108);
-})
-
-function generatePiano(from, to) {
-	let container = document.getElementById("pianoContainer");
-	let toneContainer = document.getElementById("pianoTone");
-	let semitoneContainer = document.getElementById("pianoSemitone");
-
-	for (let n = from; n <= to; n++) {
-		if (n % 2 == (n % 12 > 4)) {
-			toneContainer.innerHTML += toneKeyHTML;
-			toneContainer.lastChild.innerHTML = n;	
-		}
-		else {
-			semitoneContainer.innerHTML += semitoneKeyHTML;
-			semitoneContainer.lastChild.innerHTML = n;
-			if (n % 12 == 3 | n % 12 == 10) {
-				semitoneContainer.innerHTML += placeholderSemitoneKeyHTML;
+	
+	function generatePiano(from, to) {
+		let container = document.getElementById("pianoContainer");
+		let toneContainer = document.getElementById("pianoTone");
+		let semitoneContainer = document.getElementById("pianoSemitone");
+		
+		for (let n = from; n <= to; n++) {
+			if (n % 2 == (n % 12 > 4)) {
+				toneContainer.innerHTML += toneKeyHTML;
+				toneContainer.lastChild.innerHTML = n;	
 			}
-		}
-	}
-
-	toneContainer.querySelectorAll(".tone-key").forEach((element, index) => {
-		configureKey(element)
-	});
-
-	semitoneContainer.querySelectorAll(".semitone-key").forEach((element, index) => {
-		configureKey(element)
-	});
-
-	scrollToCenter(container);
-	new ResizeObserver(elementResize).observe(container)
-
-	container.onresize = (e) => {
-		scrollToCenter(container);
-	}
-}
-
-function scrollToCenter(element) {
-	element.scroll(element.scrollWidth / 2 - element.offsetWidth / 2, 
-			element.scrollHeight / 2 - element.offsetHeight / 2);
-	containerScroll = element.scrollWidth / 2 - element.offsetWidth / 2
-}
-
-function elementResize() {
-	scrollToCenter(document.getElementById("pianoContainer"));
-}
-
-function configureKey(element) {
-	element.addEventListener("mouseover", (e) => {
-		element.classList.add("hover");
-		if (mouseClick == true) {
-			element.classList.add("active");
-			activeKey = element.innerHTML;
+			else {
+				semitoneContainer.innerHTML += semitoneKeyHTML;
+				semitoneContainer.lastChild.innerHTML = n;
+				if (n % 12 == 3 | n % 12 == 10) {
+					semitoneContainer.innerHTML += placeholderSemitoneKeyHTML;
+				};
+			};
 		};
-	});
-	element.addEventListener("mouseenter", (e) => {
-		if (mouseClick == true) {
-			playNote(activeKey, 511);
-		}
-	})
-	element.addEventListener("mousedown", (e) => {
-		element.classList.add("active");
-		activeKey = element.innerHTML;
-		playNote(activeKey, 511);
-	});
-	element.addEventListener("mouseup", (e) => {
-		element.classList.remove("active");
-	});
-	element.addEventListener("mouseout", (e) => {
-		element.classList.remove("hover");
-		element.classList.remove("active");
-	});
-}
-
-function playNote(note, velocity) {
-	MIDI.setVolume(127, 127);
-	MIDI.noteOn(0, note, velocity, 0);
-	MIDI.noteOff(0, note, 2.0);
-}
+		
+		toneContainer.querySelectorAll(".tone-key").forEach((element, index) => {
+			configureKey(element);
+		});
+		
+		semitoneContainer.querySelectorAll(".semitone-key").forEach((element, index) => {
+			configureKey(element);
+		});
+		
+		scrollToCenter(container);
+		new ResizeObserver(elementResize).observe(container);
+		
+		container.onresize = (e) => {
+			scrollToCenter(container);
+		};
+	};
+	
+	function scrollToCenter(element) {
+		element.scroll(element.scrollWidth / 2 - element.offsetWidth / 2, 
+			element.scrollHeight / 2 - element.offsetHeight / 2);
+			containerScroll = element.scrollWidth / 2 - element.offsetWidth / 2;
+		};
+		
+		function elementResize() {
+			scrollToCenter(document.getElementById("pianoContainer"));
+		};
+		
+		function configureKey(element) {
+			element.addEventListener("mouseover", (e) => {
+				element.classList.add("hover");
+				if (mouseClick == true) {
+					element.classList.add("active");
+					activeKey = element.innerHTML;
+				};
+			});
+			element.addEventListener("mouseenter", (e) => {
+				if (mouseClick == true) {
+					playNote(activeKey, 511);
+				};
+			});
+			element.addEventListener("mousedown", (e) => {
+				element.classList.add("active");
+				activeKey = element.innerHTML;
+				playNote(activeKey, 511);
+			});
+			element.addEventListener("mouseup", (e) => {
+				element.classList.remove("active");
+			});
+			element.addEventListener("mouseout", (e) => {
+				element.classList.remove("hover");
+				element.classList.remove("active");
+			});
+		};
+		
+		function playNote(note, velocity) {
+			MIDI.setVolume(127, 127);
+			MIDI.noteOn(0, note, velocity, 0);
+			MIDI.noteOff(0, note, 2.0);
+		};
+};
