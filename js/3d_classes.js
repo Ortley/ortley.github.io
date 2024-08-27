88dd35ee786c451d6e7c00097fcb5d0443aef025
Ortley <etarlinguk@gmail.com>
2024-08-27 18:09:05 +0100

    moved the transform3d variables around and added get/set. fixed vector3 statics

--- js/3d_classes.js
+++ js/3d_classes.js
@@ -1,19 +1,22 @@
 class vector3 {
-    static zero = new vector3(0, 0, 0);
-    static axisX = new vector3(1, 0, 0);
-    static axisY = new vector3(0, 1, 0);
-    static axisZ = new vector3(0, 0, 1);
-
     constructor(x = 0, y = 0, z = 0) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.length = 0;
+        
     };
+
+    static zero = new vector3(0, 0, 0);
+    static one = new vector3(1, 1, 1);
+    static axisX = new vector3(1, 0, 0);
+    static axisY = new vector3(0, 1, 0);
+    static axisZ = new vector3(0, 0, 1);
 };
 
+
 class basis3D {
-    constructor(x = new vector3.axisX, y = new vector3.axisY, z = new vector3.axisZ) {
+    constructor(x = vector3.axisX, y = vector3.axisY, z = vector3.axisZ) {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -21,13 +24,27 @@ class basis3D {
 };
 
 class transform3D {
-    constructor(basis = new basis3D(), position = new vector3()) {
-        this.basis = basis;
-        this.position = position;
+    x; y; z; w;
+    constructor(x = vector3.axisX, y = vector3.axisY, z = vector3.axisZ, w = vector3.zero) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    };
 
-        this.x = this.basis.x;
-        this.y = this.basis.y;
-        this.z = this.basis.z;
-        this.w = this.position;
+    get basis() {
+        return new basis3D(this.x, this.y, this.z);
+    };
+    get position() {
+        return this.w;
+    };
+
+    set basis(val) {
+        this.x = val.x;
+        this.y = val.y;
+        this.z = val.z;
+    };
+    set position(val) {
+        this.w = val;
     };
 };
